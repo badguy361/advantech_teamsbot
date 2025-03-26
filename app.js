77a647fa36980a50bc5e6c5d910747f4e2eed485
Chih Yu Chang <43988698+badguy361@users.noreply.github.com>
2025-03-26 17:20:52 +0800

    prevent user not exist in subscription or not install agent

--- app.js
+++ app.js
@@ -21,10 +21,12 @@ let botFrameworkAuthentication;
 let MicrosoftAppId;
 let MicrosoftAppPassword;
 let MicrosoftTenantId;
+let NOTIFICATION_ENDPOINT;
 if (ENVIRONMENT === 'production') {
     MicrosoftAppId = process.env.MicrosoftAppId;
     MicrosoftAppPassword = process.env.MicrosoftAppPassword;
     MicrosoftTenantId = process.env.MicrosoftTenantId;
+    NOTIFICATION_ENDPOINT = process.env.NOTIFICATION_ENDPOINT;
     botFrameworkAuthentication = new ConfigurationBotFrameworkAuthentication({
         MicrosoftAppId: MicrosoftAppId,
         MicrosoftAppPassword: MicrosoftAppPassword,
@@ -34,6 +36,7 @@ if (ENVIRONMENT === 'production') {
 } else if (ENVIRONMENT === 'staging') {
     MicrosoftAppId = process.env.TestMicrosoftAppId;
     MicrosoftAppPassword = process.env.TestMicrosoftAppPassword;
+    NOTIFICATION_ENDPOINT = process.env.NOTIFICATION_ENDPOINT;
     botFrameworkAuthentication = new ConfigurationBotFrameworkAuthentication({
         MicrosoftAppId: MicrosoftAppId,
         MicrosoftAppPassword: MicrosoftAppPassword,
@@ -42,6 +45,7 @@ if (ENVIRONMENT === 'production') {
 } else if (ENVIRONMENT === 'local') {
     MicrosoftAppId = process.env.TestMicrosoftAppId;
     MicrosoftAppPassword = process.env.TestMicrosoftAppPassword;
+    NOTIFICATION_ENDPOINT = process.env.TEST_NOTIFICATION_ENDPOINT;
     botFrameworkAuthentication = new ConfigurationBotFrameworkAuthentication({
         MicrosoftAppId: MicrosoftAppId,
         MicrosoftAppPassword: MicrosoftAppPassword,
@@ -270,7 +274,13 @@ class TeamsBot extends ActivityHandler {
     }
 }
 
-// Save user to DB
+/**
+ * save user subscriptions to DB
+ * @param {string} user_name - user name
+ * @param {string} user_id - user id
+ * @param {object} conversation_reference - conversation reference
+ * @param {array} subscriptions - subscriptions
+ */
 async function saveUserSubscriptionsToDB(userName, userId, conversationReference, subscriptions) {
     try {
         const database = DBClient.database(cosmosDBDatabaseId);
@@ -296,7 +306,93 @@ async function saveUserSubscriptionsToDB(userName, userId, conversationReference
     }
 }
 
-// API Key validation middleware
+/**
+ * check user non existing in DB
+ * @param {Array} userNames - user names
+ * @returns {Promise<Array>}
+ */
+async function checkUserNonExisting(userNames) {
+    try {
+        const database = DBClient.database(cosmosDBDatabaseId);
+        const container = database.container(cosmosDBContainerId);
+
+        // look up user names in DB
+        const querySpec = {
+            query: `
+                SELECT c.name 
+                FROM c 
+                WHERE ARRAY_CONTAINS(@userNames, c.name)
+            `,
+            parameters: [
+                {
+                    name: "@userNames",
+                    value: userNames
+                }
+            ]
+        };
+
+        const { resources: existingUsers } = await container.items.query(querySpec).fetchAll();
+        const existingUserNames = existingUsers.map(user => user.name);
+
+        const nonExistingUsers = userNames.filter(name => !existingUserNames.includes(name));
+
+        console.log("nonExistingUsers", nonExistingUsers);
+        return nonExistingUsers;
+
+    } catch (error) {
+        console.error("檢查用戶是否存在時發生錯誤:", error);
+        throw error;
+    }
+}
+
+/**
+ * check user subscription
+ * @param {Array} userNames - user names
+ * @param {string} jobName - job name
+ * @returns {Promise<Array>}
+ */
+async function checkUserSubscription(userNames, jobName) {
+    try {
+        const database = DBClient.database(cosmosDBDatabaseId);
+        const container = database.container(cosmosDBContainerId);
+
+        // look up user names in DB
+        const querySpec = {
+            query: `
+                SELECT c.name, c.subscriptions
+                FROM c
+                WHERE ARRAY_CONTAINS(@userNames, c.name)
+            `,
+            parameters: [
+                {
+                    name: "@userNames",
+                    value: userNames
+                }
+            ]
+        };
+
+        const { resources: users } = await container.items.query(querySpec).fetchAll();
+
+        // filter out users who do not have `jobName` in their subscriptions
+        const isNotSubscribedUsers = users
+            .filter(user => !user.subscriptions || !user.subscriptions.includes(jobName))
+            .map(user => user.name);
+
+        console.log("isNotSubscribedUsers", isNotSubscribedUsers);
+        return isNotSubscribedUsers;
+
+    } catch (error) {
+        console.error(`檢查用戶訂閱狀態時發生錯誤: ${error.message}`);
+        throw error;
+    }
+}
+
+/**
+ * API Key validation middleware
+ * @param {object} req - request object
+ * @param {object} res - response object
+ * @param {function} next - next middleware function
+ */
 function validateApiKey(req, res, next) {
     const apiKey = req.headers['scm_api_key'];
     const expectedApiKey = process.env.SCM_API_KEY;
@@ -308,7 +404,12 @@ function validateApiKey(req, res, next) {
     return next();
 }
 
-// CORS middleware
+/**
+ * CORS middleware
+ * @param {object} req - request object
+ * @param {object} res - response object
+ * @param {function} next - next middleware function
+ */
 function corsMiddleware(req, res, next) {
     res.header('Access-Control-Allow-Origin', 'xxx');
     res.header('Access-Control-Allow-Methods', 'POST, OPTIONS');
@@ -330,30 +431,53 @@ server.use(corsMiddleware);
 
 // Add proactive notification endpoint
 server.post('/api/notifications', validateApiKey, async (req, res) => {
-    const job_name = req.body['job_name'];
-    let user_name;
+    const jobName = req.body['job_name'];
+    let userNames;
     let message;
-    if (job_name === 'pullin') {
+    if (jobName === 'pull_in') {
         const jobId = req.body['job_id'];
         if (!jobId) {
             res.send(400, 'job_id is required');
             return;
         }
-        user_name = ['Joey.Chang']; // TODO: use jobid to get userName from HANA
-        message = 'pullin'; // TODO: usee jobid get message from HANA
-    } else if (job_name === 'customer') {
-        user_name = ['Joey.Chang']; // TODO: get userName(person in charge) from cosmos DB
-        message = 'customer'; // TODO: get message through SIS from SQL server
+        userNames = ['Joey.Chang', 'Tina.Chen']; // TODO: use jobid to get userName from HANA
+        message = 'pull_in'; // TODO: usee jobid get message from HANA
+    } else if (jobName === 'LTB_customer') {
+        userNames = ['Joey.Chang', 'Tina.Chen']; // TODO: get userName(person in charge) from cosmos DB
+        message = 'LTB_customer'; // TODO: get message through SIS from SQL server
     }
 
-    if (!user_name || !message) {
-        res.send(400, 'user_name and message are not found');
+    if (!userNames || !message) {
+        res.send(400, 'job_name is not found');
         return;
     }
 
-    const NOTIFICATION_ENDPOINT = process.env.NOTIFICATION_ENDPOINT;
-    await axios.post(NOTIFICATION_ENDPOINT, { user_name, message });
-    res.send(200, '通知已發送');
+    // check user is existing in DB and subscribed to the job
+    const isNotExistingUsers = await checkUserNonExisting(userNames);
+    const isNotSubscribedUsers = await checkUserSubscription(userNames, jobName);
+    const validUserNames = userNames.filter(userName => 
+        !isNotExistingUsers.includes(userName) && 
+        !isNotSubscribedUsers.includes(userName)
+    );
+
+    if (validUserNames.length > 0) {
+        await axios.post(NOTIFICATION_ENDPOINT, { userNames: validUserNames, message });
+    }
+
+    if (isNotExistingUsers.length > 0 || isNotSubscribedUsers.length > 0) {
+        const messageLines = ['通知已發送。'];
+        if (isNotExistingUsers.length > 0) {
+            messageLines.push(`以下用戶未安裝 SCM Agent: ${isNotExistingUsers.join(', ')}。`);
+        }
+        if (isNotSubscribedUsers.length > 0) {
+            messageLines.push(`以下用戶未訂閱 ${jobName} 服務: ${isNotSubscribedUsers.join(', ')}。`);
+        }
+
+        const message = messageLines.join(' ');
+        res.send(200, message);
+    } else {
+        res.send(200, '通知已發送');
+    }
 });
 
 // Configure server message endpoint
